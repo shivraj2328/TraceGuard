26eb71b2498b3774537a630c7391f78277d1ca5a
shivraj2328 <shivrajjagtap6@gmail.com>
2026-09-11 16:42:56 +0530

    feat: implement pino logger

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 
+const { logger } = require('./utils/logger'); 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
